3fa6eaae37bf60ac3c9294a98c50df2d0b2f15d3
maximsibirczev <maxim.sibirczev@gmail.com>
2019-11-13 00:05:47 +0400

    users rest api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'social-net';
+  users=[];
+
+
+	constructor(private userService: UserService) {
+	//	this.users = this.userService.users
+	 }
+
+	  ngOnInit(){  
+	  	this.userService.getUsers().subscribe((users)=>{
+	  		console.log(users)
+	  		this.users=users;
+	  	})
+    //    this.httpService.getData().subscribe((data:User) => this.user=data);
+    }
 }
