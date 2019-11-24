4989506ec903d764eb47b412e80b5275a7cd1d89
sibircev <sibircev@galaktika-it.ru>
2019-11-24 20:16:40 +0300

    fix bag tutorial

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -18,11 +18,11 @@ export class UserService {
   }
 
   getUsers(): Observable<any> {
-    return this.http.get('https://randomuser.me/api/?inc=gender,name,picture,location&results=' + this.size + '&nat=gb')
+    return this.http.get<any>('https://randomuser.me/api/?inc=gender,name,picture,location&results=' + this.size + '&nat=gb')
       .pipe(
         map(response => {
           this.results = response.results;
-          return this.results.map(function (user: any) {
+          return this.results.map(function(user: any) {
             return {
               name: user.name.first + ' ' + user.name.last,
               image: user.picture.large,
